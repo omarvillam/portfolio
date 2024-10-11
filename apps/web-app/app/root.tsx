768499935d22390b9ad75d5669579e2144f559ab
Omar Villalobos <ovillalobos@synctree.com>
2024-10-11 17:48:47 -0300

    changed head title depending on lang

--- apps/web-app/app/root.tsx
+++ apps/web-app/app/root.tsx
@@ -8,6 +8,7 @@ import {
 } from '@remix-run/react';
 
 import TailwindStyles from "./tailwind.css?url";
+import {trans} from "./lib/lang";
 
 export const links = () => [
   { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" },
@@ -24,7 +25,7 @@ export function Layout() {
     <head>
       <meta charSet="utf-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <title>Omar Villalobos - Software Developer</title>
+      <title>{trans("meta.title")}</title>
 
       <meta property="og:title" content="Omar Villalobos - Software Developer"/>
       <meta property="og:description"
