2877e93df26bb4f5d952212cf3aa44f7243e0fb1
Omar Villalobos <omarvilla0406@gmail.com>
2025-01-23 15:27:43 -0300

    attempt to fix vercel deployment

--- apps/web-app/vite.config.ts
+++ apps/web-app/vite.config.ts
@@ -3,14 +3,18 @@ import {defineConfig} from "vite";
 import {installGlobals} from "@remix-run/node";
 import {nxCopyAssetsPlugin} from "@nx/vite/plugins/nx-copy-assets.plugin";
 import {nxViteTsPaths} from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
+import {vercelPreset} from "@vercel/remix/vite";
 
 installGlobals();
 
 export default defineConfig({
   server: {port: 3000, fs: {allow: ['../..']}},
-  plugins: [remix({
-    ssr: false
-  }), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  plugins: [
+    remix({ssr: false}),
+    nxViteTsPaths(),
+    nxCopyAssetsPlugin(['*.md']),
+    vercelPreset(),
+  ],
   ssr: {
     noExternal: [/^@portfolio\//]
   }
